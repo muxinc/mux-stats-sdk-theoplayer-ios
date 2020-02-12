174708091b6a7e680bfad44aef5bed2e17fccbfa
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-02-11 17:03:25 -0800

    cleanup DemoApp and install Mux-Stats-THEOplayer from cocoapods

--- apps/DemoApp/DemoApp/ViewController.swift
+++ apps/DemoApp/DemoApp/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var player: THEOplayer!
 
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         self.player = THEOplayer(configuration: THEOplayerConfiguration(chromeless: false))
         self.player.frame = view.bounds
         self.player.addAsSubview(of: view)
@@ -24,13 +25,13 @@ class ViewController: UIViewController {
             src: "https://stream.mux.com/tqe4KzdxU6GLc8oowshXgm019ibzhEX3k.m3u8",
             type: "application/vnd.apple.mpegurl")
 
-//        let ad = THEOAdDescription(src: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=")
+        let ad = THEOAdDescription(src: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=")
 
-        let source = SourceDescription(source: typedSource, ads: [], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
+        let source = SourceDescription(source: typedSource, ads: [ad], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
         self.player.source = source
         
         // TODO: Add your property key!
-        let playerData = MUXSDKCustomerPlayerData(environmentKey: "YOUR_ENVIRONMENT_KEY")!
+        let playerData = MUXSDKCustomerPlayerData(environmentKey: "YOUR_ENV_KEY")!
 
         let videoData = MUXSDKCustomerVideoData()
         videoData.videoTitle = "Big Buck Bunny"
@@ -40,21 +41,21 @@ class ViewController: UIViewController {
         MUXSDKStatsTHEOplayer.monitorTHEOplayer(self.player, name: playerName, playerData: playerData, videoData: videoData, softwareVersion: "1.1.1")
         self.player.play()
 
-        // After 90 seconds, we'll change the video.
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(110)) {
-            let videoData = MUXSDKCustomerVideoData()
-            videoData.videoTitle = "Apple Keynote"
-            videoData.videoId = "applekeynote2010"
-
-            MUXSDKStatsTHEOplayer.videoChangeForPlayer(name: self.playerName, videoData: videoData)
-
-            let typedSource = TypedSource(
-                src: "https://stream.mux.com/tNrV028WTqCOa02zsveBdNwouzgZTbWx5x.m3u8",
-                type: "application/vnd.apple.mpegurl")
-
-            let source = SourceDescription(source: typedSource, ads: [], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
-            self.player.source = source
-            self.player.play()
-        }
+        // Example of changing the video after 60 seconds
+//        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(60)) {
+//            let videoData = MUXSDKCustomerVideoData()
+//            videoData.videoTitle = "Apple Keynote"
+//            videoData.videoId = "applekeynote2010"
+//
+//            MUXSDKStatsTHEOplayer.videoChangeForPlayer(name: self.playerName, videoData: videoData)
+//
+//            let typedSource = TypedSource(
+//                src: "https://stream.mux.com/tNrV028WTqCOa02zsveBdNwouzgZTbWx5x.m3u8",
+//                type: "application/vnd.apple.mpegurl")
+//
+//            let source = SourceDescription(source: typedSource, ads: [], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
+//            self.player.source = source
+//            self.player.play()
+//        }
     }
 }
