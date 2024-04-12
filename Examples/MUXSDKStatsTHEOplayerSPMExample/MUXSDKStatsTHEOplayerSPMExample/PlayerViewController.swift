4e8a65db8d35324b02905de15218d1f03689fe20
AJ Lauer Barinov <abarinov@mux.com>
2024-04-12 11:01:17 -0700

    remove deprecated API usage

--- Examples/MUXSDKStatsTHEOplayerSPMExample/MUXSDKStatsTHEOplayerSPMExample/PlayerViewController.swift
+++ Examples/MUXSDKStatsTHEOplayerSPMExample/MUXSDKStatsTHEOplayerSPMExample/PlayerViewController.swift
@@ -69,9 +69,9 @@ class PlayerViewController: UIViewController {
             type: "application/vnd.apple.mpegurl"
         )
 
-        let ad = THEOAdDescription(src: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=")
-
-        let source = SourceDescription(source: typedSource, ads: [ad], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
+        let source = SourceDescription(
+            source: typedSource
+        )
         self.player.source = source
 
         // TODO: Add your property key!
