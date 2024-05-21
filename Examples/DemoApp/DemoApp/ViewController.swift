74fadecbe53632e5532d3b764953a026efe067ac
AJ Lauer Barinov <abarinov@mux.com>
2024-05-21 08:41:48 -0700

    build: misc example app fixes

--- Examples/DemoApp/DemoApp/ViewController.swift
+++ Examples/DemoApp/DemoApp/ViewController.swift
@@ -11,25 +11,43 @@ import MuxStatsTHEOplayer
 import THEOplayerSDK
 import UIKit
 
+extension ProcessInfo {
+    var theoPlayerLicenseKey: String {
+        environment["THEOPLAYER_LICENSE_KEY"] ?? ""
+    }
+
+    var environmentKey: String {
+        environment["MUX_ENVIRONMENT_KEY"] ?? ""
+    }
+}
+
 class ViewController: UIViewController {
     let playerName = "demoplayer"
     var player: THEOplayer!
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.player = THEOplayer(configuration: THEOplayerConfiguration(chromeless: false))
+        // THEOplayer object
+        self.player = {
+            let builder = THEOplayerConfigurationBuilder()
+            builder.license = ProcessInfo.processInfo.theoPlayerLicenseKey
+            return THEOplayer(
+                configuration: builder.build()
+            )
+        }()
         self.player.frame = view.bounds
         self.player.addAsSubview(of: view)
 
         let typedSource = TypedSource(
             src: "https://stream.mux.com/tqe4KzdxU6GLc8oowshXgm019ibzhEX3k.m3u8",
-            type: "application/vnd.apple.mpegurl")
+            type: "application/vnd.apple.mpegurl"
+        )
 
-        let ad = THEOAdDescription(src: "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpostpod&cmsid=496&vid=short_onecue&correlator=")
-
-        let source = SourceDescription(source: typedSource, ads: [ad], textTracks: nil, poster: nil, analytics: nil, metadata: nil)
+        let source = SourceDescription(
+            source: typedSource
+        )
         self.player.source = source
-        
+
         // TODO: Add your property key!
         let playerData = MUXSDKCustomerPlayerData(environmentKey: "YOUR_ENV_KEY")!
 
