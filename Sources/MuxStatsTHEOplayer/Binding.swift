099d499e994290205387b6326ca43f3866f25739
Kevin Pittevils <kevin.pittevils@androme.be>
2026-06-19 13:53:35 +0200

    fix: crash https://github.com/muxinc/mux-stats-sdk-theoplayer-ios/issues/39

--- Sources/MuxStatsTHEOplayer/Binding.swift
+++ Sources/MuxStatsTHEOplayer/Binding.swift
@@ -140,7 +140,9 @@ fileprivate extension Binding {
         data.playerHeight = player.frame.size.height * UIScreen.main.nativeScale as NSNumber
         data.playerIsFullscreen = player.frame.equalTo(UIScreen.main.bounds) ? "true" : "false"
         data.playerIsPaused = NSNumber(booleanLiteral: player.paused)
-        data.playerPlayheadTime = NSNumber(value: (Int64)(player.currentTime * 1000))
+        if player.currentTime.isFinite {
+            data.playerPlayheadTime = NSNumber(value: (Int64)(player.currentTime * 1000))
+        }
         return data
     }
 
