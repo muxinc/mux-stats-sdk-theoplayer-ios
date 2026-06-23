c9dbc7d885a7bd8d3701ba4c4d3f3bcf40c233b6
Kevin Pittevils <kevin-kp@users.noreply.github.com>
2026-06-23 19:47:14 +0200

    chore: update THEOplayer and MuxStatsCore (#43)

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "Mux-Stats-THEOplayer",
     platforms: [
-        .iOS(.v13),
-        .tvOS(.v13)
+        .iOS(.v15),
+        .tvOS(.v15)
     ],
     products: [
         .library(
@@ -21,7 +21,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/THEOplayer/theoplayer-sdk-apple.git",
-            .upToNextMajor(from: "10.0.0")
+            .upToNextMajor(from: "11.0.0")
         )
     ],
     targets: [
